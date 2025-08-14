ccd7d50a047b1ecf3746a2a12461c5ef2bf71773
Christian Merten <christian@merten.dev>
2025-08-15 01:26:33 +0200

    more logic exercises

--- UuGeometry2025/Exercises/02Logic.lean
+++ UuGeometry2025/Exercises/02Logic.lean
@@ -1,7 +1,7 @@
-section
+import Mathlib
 
 /-!
-# Logic exercises
+# Logic and sets exercises
 
 Try and fill in the following `sorry`s.
 -/
@@ -30,3 +30,107 @@ example (α β : Type) (p : α → β → Prop) (h : ∃ x, ∀ y, p x y) : ∀ 
 
 example (α : Type) (p : α → Prop) (h : ∀ x, ¬ p x) : ¬ ∃ x, p x :=
   sorry
+
+section
+
+/-!
+# Set exercises
+
+Try and fill in the following `sorry`s.
+-/
+
+variable {α β : Type*} (s t u : Set α)
+
+example : s ∩ (t ∪ u) ⊆ s ∩ t ∪ s ∩ u := sorry
+
+example : s ∩ t ∪ s ∩ u ⊆ s ∩ (t ∪ u) := sorry
+
+example : (s \ t) \ u ⊆ s \ (t ∪ u) := sorry
+
+example : s \ (t ∪ u) ⊆ (s \ t) \ u := sorry
+
+example : s ∩ t = t ∩ s := sorry
+
+example : s ∪ s = s := sorry
+
+example : s ∪ s ∩ t = s := sorry
+
+variable {I : Type*}
+variable (A B : I → Set α)
+
+/-
+Not only can we take the union of sets, but given some indexing type `I` and
+a family of sets `A : I → Set α`, we can take the *indexed union* `⋃ i : I, A i`.
+
+Note: this is a different symbol than the usual union, and is written with `\U` or
+`\Union` (note these both have capital Us).
+
+We have something very similar with intersection, with the big `⋂` being written `\I`
+or `\Inter`.
+-/
+
+#check Set.mem_iUnion
+#check Set.mem_iInter
+
+example : (s ∩ ⋃ i, A i) = ⋃ i, A i ∩ s :=
+  sorry
+
+example : (s ∪ ⋂ i, A i) = ⋂ i, A i ∪ s :=
+  sorry
+
+end
+
+section
+variable {α β : Type*}
+variable (f : α → β)
+variable (s t : Set α)
+variable (u v : Set β)
+
+example : f ⁻¹' (u ∩ v) = f ⁻¹' u ∩ f ⁻¹' v :=
+  sorry
+
+example : f '' (s ∪ t) = f '' s ∪ f '' t :=
+  sorry
+
+example : s ⊆ f ⁻¹' (f '' s) :=
+  sorry
+
+example : f '' s ⊆ v ↔ s ⊆ f ⁻¹' v :=
+  sorry
+
+/-
+We have not yet seen injective (or surjective) functions. Control click (or command click
+on mac) on `Injective` to jump to the definition.
+
+Note that these are both defined as forall statements, so we can use `specialize` and
+`intro` on them directly without even unfolding them.
+-/
+example (h : Function.Injective f) : f ⁻¹' (f '' s) ⊆ s := by sorry
+
+example : f '' (f ⁻¹' u) ⊆ u := sorry
+
+example (h : Function.Surjective f) : u ⊆ f '' (f ⁻¹' u) := sorry
+
+example (h : s ⊆ t) : f '' s ⊆ f '' t := sorry
+
+example (h : u ⊆ v) : f ⁻¹' u ⊆ f ⁻¹' v := sorry
+
+example : f ⁻¹' (u ∪ v) = f ⁻¹' u ∪ f ⁻¹' v := sorry
+
+example : f '' (s ∩ t) ⊆ f '' s ∩ f '' t := sorry
+
+example (h : Function.Injective f) : f '' s ∩ f '' t ⊆ f '' (s ∩ t) := sorry
+
+example : f '' s \ f '' t ⊆ f '' (s \ t) := sorry
+
+example : f ⁻¹' u \ f ⁻¹' v ⊆ f ⁻¹' (u \ v) := sorry
+
+example : f '' s ∩ v = f '' (s ∩ f ⁻¹' v) := sorry
+
+example : f '' (s ∩ f ⁻¹' u) ⊆ f '' s ∩ u := sorry
+
+example : s ∩ f ⁻¹' u ⊆ f ⁻¹' (f '' s ∩ u) := sorry
+
+example : s ∪ f ⁻¹' u ⊆ f ⁻¹' (f '' s ∪ u) := sorry
+
+end
